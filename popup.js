31ec53482e13880398cffdf0047cb74596274082
Jacob Teves <jacobteves@gmail.com>
2016-03-21 23:35:05 -0400

    First commit of the Capital One Chrome Extension Application

---
+++ popup.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'http://gtmetrix.com/analyze.html?bm';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var checkUrlButton = document.getElementById('checkUrl');
+  checkUrlButton.addEventListener('click', summarize, false);
+}, false);
+
+function summarize(){
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'http://gtmetrix.com/analyze.html?bm';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = d.getElementById('url').value;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }
