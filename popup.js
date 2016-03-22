4d80e23fd67b1d3968a5f28f0d7d0b157029ee8a
Jacob Teves <jacobteves@gmail.com>
2016-03-22 17:54:32 -0400

    Set up a skeleton of summarizing a text. Need to use API properly still

--- popup.js
+++ popup.js
@@ -1,42 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
+  checkPageButton.addEventListener('click', summarizeThis, false);
 }, false);
 
 document.addEventListener('DOMContentLoaded', function() {
   var checkUrlButton = document.getElementById('checkUrl');
-  checkUrlButton.addEventListener('click', summarize, false);
+  // console.log("URL Tester: " + document.getElementById('url').value);
+  checkUrlButton.addEventListener('click', summarizeURL, false);
 }, false);
 
-function summarize(){
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+//Summarize input URL
+function summarizeURL(){
+  chrome.tabs.getSelected(null, function(tab) {
+    d = document;
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = d.getElementById('url').value;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }
+    var f = d.createElement('form');
+    f.action = 'http://textsummarization.net/text-summarizer';
+    f.method = 'post';
+    var i = d.createElement('input');
+    i.type = 'hidden';
+    i.name = 'url';
+    i.value = d.getElementById('url').value;
+    f.appendChild(i);
+    d.body.appendChild(f);
+    f.submit();
+  });
+}
+
+//Summarize current page
+function summarizeThis() {
+
+  chrome.tabs.getSelected(null, function(tab) {
+    d = document;
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "http://textsummarization.net/text-summarizer", false);
+// xhr.send();
+// console.log(xhr.status);
+    var f = d.createElement('form');
+    f.action = 'http://textsummarization.net/text-summarizer';
+    f.method = 'post';
+    var i = d.createElement('input');
+    i.type = 'hidden';
+    i.name = 'url';
+    i.value = tab.url;
+    f.appendChild(i);
+    d.body.appendChild(f);
+    if (f.onsubmit())  f.submit();
+
+  });
+}
